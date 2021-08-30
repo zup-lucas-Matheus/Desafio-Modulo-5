78fbf065071cf44c412005a7273553ce31a03511
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 10:39:09 -0300

    Metódo para trazer todos os usuarios salvos no BD.

--- src/main/java/br/com/example/zup_face/usuario/UsuarioService.java
+++ src/main/java/br/com/example/zup_face/usuario/UsuarioService.java
@@ -3,6 +3,8 @@ package br.com.example.zup_face.usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UsuarioService {
 
@@ -12,6 +14,11 @@ public class UsuarioService {
     //Metódo para cadastrar usuario.
     public Usuario saveUsuario(Usuario usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    //Metódo para trazer todos os usuarios.
+    public List<Usuario> allUsuario(){
+        return (List<Usuario>) usuarioRepository.findAll();
     }
 
 }
