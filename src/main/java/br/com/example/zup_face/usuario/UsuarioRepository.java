ad0ad905d5e5314ce7d17119f7bedeb126b45d03
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 10:25:06 -0300

    Classe Repository criada, com o CRUD de Usuario e o id que será o email.

---
+++ src/main/java/br/com/example/zup_face/usuario/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.example.zup_face.usuario;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+
+}
