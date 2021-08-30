8b3f06f413189b427beccd6d599a178852e13083
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 17:34:25 -0300

    Classe validação criada com @Bean

---
+++ src/main/java/br/com/example/zup_face/validacao/Validacao.java
@@ -0,0 +1,26 @@
+package br.com.example.zup_face.validacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+public class Validacao {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+}
