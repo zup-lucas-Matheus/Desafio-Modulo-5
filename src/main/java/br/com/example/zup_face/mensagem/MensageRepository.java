a31a164cc601d256c423b226ec20cee47cf0cd7b
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 11:26:42 -0300

    Repository da Classe Mensagem criada.

---
+++ src/main/java/br/com/example/zup_face/mensagem/MensageRepository.java
@@ -0,0 +1,7 @@
+package br.com.example.zup_face.mensagem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MensageRepository extends CrudRepository<Mensagem, Integer> {
+
+}
