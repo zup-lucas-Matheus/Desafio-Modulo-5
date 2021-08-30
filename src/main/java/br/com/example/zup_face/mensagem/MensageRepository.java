7c9314f83cf497872779060e08235b7232b13ad2
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 12:23:00 -0300

    Crud para trazer a quantidade de mensagens não vizualizadas dos usuarios do Banco de Dados.

--- src/main/java/br/com/example/zup_face/mensagem/MensageRepository.java
+++ src/main/java/br/com/example/zup_face/mensagem/MensageRepository.java
@@ -1,7 +1,12 @@
 package br.com.example.zup_face.mensagem;
 
+import br.com.example.zup_face.enums.Visualizado;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MensageRepository extends CrudRepository<Mensagem, Integer> {
+
+    List<Mensagem> findAllByEmailDestinoEmailAndVisualizado(String email, Visualizado visualizado);
 
 }
