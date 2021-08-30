61c21b59fbba5f264d446b5e71680c56a8bea2f1
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 12:32:41 -0300

    Classe dto para mostrar a quantidade de mensagens não vizualizadas em JSON.

---
+++ src/main/java/br/com/example/zup_face/dto/QuantidadeMsnNaoLida.java
@@ -0,0 +1,12 @@
+package br.com.example.zup_face.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class QuantidadeMsnNaoLida {
+
+    private int mensagemNaoView;
+
+}
