38c81947ad980cf7b9b6d886cf2a370bbc74df5e
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 11:50:28 -0300

    Classe Mensagemcontroller criada.

---
+++ src/main/java/br/com/example/zup_face/mensagem/MensagemController.java
@@ -0,0 +1,15 @@
+package br.com.example.zup_face.mensagem;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("mensagens")
+public class MensagemController {
+
+
+
+
+
+}
