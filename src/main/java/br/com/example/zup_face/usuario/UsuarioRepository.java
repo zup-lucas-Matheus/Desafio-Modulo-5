6c329b083a29805a8528f519764325c9dd248e89
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 10:50:01 -0300

    Crud para verificar se o usuario já existe pelo id email.

--- src/main/java/br/com/example/zup_face/usuario/UsuarioRepository.java
+++ src/main/java/br/com/example/zup_face/usuario/UsuarioRepository.java
@@ -2,6 +2,10 @@ package br.com.example.zup_face.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+
+    Optional<Usuario> findByEmail(String email);
 
 }
