0de4bbf7a07b2da3b00ed32d4948f3e53ce109cd
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 17:47:50 -0300

    Adicionando os atributos das mensagens da exeção que vai aparecer para o usuario.

--- src/main/java/br/com/example/zup_face/exeptions/ManipuladorDeExcecoes.java
+++ src/main/java/br/com/example/zup_face/exeptions/ManipuladorDeExcecoes.java
@@ -1,4 +1,15 @@
 package br.com.example.zup_face.exeptions;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class ManipuladorDeExcecoes {
+
+    private int status;
+    private String mensagem;
+    private String titulo;
+
+
 }
