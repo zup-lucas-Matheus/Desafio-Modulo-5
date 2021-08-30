b4021acce16f7c201ac9676b9579aafd6f8832bc
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 11:23:13 -0300

    Classe Mensagens criada com seus atributos.

---
+++ src/main/java/br/com/example/zup_face/mensagem/Mensagem.java
@@ -0,0 +1,27 @@
+package br.com.example.zup_face.mensagem;
+
+
+import br.com.example.zup_face.usuario.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "mensagens")
+public class Mensagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "user_origem")
+    private Usuario emailOrigem;
+    @JoinColumn(name = "user_destino")
+    private Usuario emailDestino;
+
+
+}
