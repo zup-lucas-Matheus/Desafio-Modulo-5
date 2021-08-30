6dadc8ffcaeff09e038ded090ea88f3b7e0fd224
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 11:52:00 -0300

    Injeção da mensagemService na minha classe controller.

--- src/main/java/br/com/example/zup_face/mensagem/MensagemController.java
+++ src/main/java/br/com/example/zup_face/mensagem/MensagemController.java
@@ -1,6 +1,8 @@
 package br.com.example.zup_face.mensagem;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("mensagens")
 public class MensagemController {
 
+    @Autowired
+    private MensagemService mensagemService;
 
 
 
