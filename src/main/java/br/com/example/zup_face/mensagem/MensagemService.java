47a8c2ca57a15b86c2a5ddf11b5f6c26d84db314
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 11:30:20 -0300

    Injeção da do service do Usuario na service da mensagem.

--- src/main/java/br/com/example/zup_face/mensagem/MensagemService.java
+++ src/main/java/br/com/example/zup_face/mensagem/MensagemService.java
@@ -1,5 +1,6 @@
 package br.com.example.zup_face.mensagem;
 
+import br.com.example.zup_face.usuario.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,10 @@ public class MensagemService {
 
     @Autowired
     private MensageRepository mensageRepository;
+    @Autowired
+    private UsuarioService usuarioService;
+
+
+
 
 }
