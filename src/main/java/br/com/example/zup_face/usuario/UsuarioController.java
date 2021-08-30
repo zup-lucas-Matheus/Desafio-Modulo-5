284f35a4e3ce7140d9724d4374c67bd38056efd4
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 10:36:32 -0300

    Metódo para cadastrar usuario trazendo o serviço da classe UsuarioService.

--- src/main/java/br/com/example/zup_face/usuario/UsuarioController.java
+++ src/main/java/br/com/example/zup_face/usuario/UsuarioController.java
@@ -1,6 +1,8 @@
 package br.com.example.zup_face.usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,11 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @PostMapping
+    public Usuario saveUsuario(@RequestBody Usuario usuario){
+        return usuarioService.saveUsuario(usuario);
+    }
 
 
 }
