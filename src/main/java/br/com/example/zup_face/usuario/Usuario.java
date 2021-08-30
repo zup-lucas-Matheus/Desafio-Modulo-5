96f087bb7c50feac6ecf336ddc4ecf0ccedaa4c8
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 17:33:34 -0300

    Adicionando as validações a classe usuario.

--- src/main/java/br/com/example/zup_face/usuario/Usuario.java
+++ src/main/java/br/com/example/zup_face/usuario/Usuario.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,8 +20,12 @@ import javax.persistence.Table;
 public class Usuario {
 
     @Id
+    @Email(message = "{validacao.email.obrigatório}")
     private String email;
+    @NotBlank(message = "{validacao.nome.obrigatorio}")
+    @Size(min = 2, max = 20)
     private String nome;
+    @Size(min = 2, max = 20)
     private String sobrenome;
     private Cargo cargo;
 
