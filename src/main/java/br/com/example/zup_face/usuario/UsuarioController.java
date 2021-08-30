2f094189ab8f740b0e3d5a288819a1fc5eff4203
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 10:29:36 -0300

    Classe controller criada, para receber os serviços da minha classe service.

---
+++ src/main/java/br/com/example/zup_face/usuario/UsuarioController.java
@@ -0,0 +1,13 @@
+package br.com.example.zup_face.usuario;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+
+
+
+
+}
