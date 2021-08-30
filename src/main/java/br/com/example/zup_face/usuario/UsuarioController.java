4994c3846903f6e726b63bb70a8b28315ab6da8d
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 10:30:32 -0300

    Injeção da minha classe UsuarioService na classe controller

--- src/main/java/br/com/example/zup_face/usuario/UsuarioController.java
+++ src/main/java/br/com/example/zup_face/usuario/UsuarioController.java
@@ -1,5 +1,6 @@
 package br.com.example.zup_face.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsuarioController {
 
-
+    @Autowired
+    private UsuarioService usuarioService;
 
 
 }
