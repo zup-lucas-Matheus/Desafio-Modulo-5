c1c2f493f3e0c74c7f0595587ab664ed75f6a606
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 11:16:24 -0300

    Adicionando o @Bean para o spring boot considerar o ModelMapper

--- src/main/java/br/com/example/zup_face/dto/componente/Conversor.java
+++ src/main/java/br/com/example/zup_face/dto/componente/Conversor.java
@@ -1,11 +1,12 @@
 package br.com.example.zup_face.dto.componente;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Conversor {
-
+    @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
