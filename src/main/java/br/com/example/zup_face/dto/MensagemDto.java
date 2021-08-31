d3b46e42dcceee4cb3bab762cf4159073d7e8236
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 21:47:31 -0300

    Adicionando validação de email origem e destino.

--- src/main/java/br/com/example/zup_face/dto/MensagemDto.java
+++ src/main/java/br/com/example/zup_face/dto/MensagemDto.java
@@ -1,10 +1,25 @@
 package br.com.example.zup_face.dto;
 
+import br.com.example.zup_face.enums.Visualizado;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+
 public class MensagemDto {
 
-
+    @Email(message = "Email origem é obrigatorio")
     private String emailOrigem;
     private String mensagem;
+    @Email(message = "Email destino é obrigatorio")
     private String emailDestino;
+    private Visualizado visualizado;
+    private LocalDateTime date;
 
 }
