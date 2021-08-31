c93d005bde5e1a5311bf6adac52622a54f432035
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-30 21:46:41 -0300

    Adicionando validação

--- src/main/java/br/com/example/zup_face/dto/UsuarioDto.java
+++ src/main/java/br/com/example/zup_face/dto/UsuarioDto.java
@@ -1,14 +1,20 @@
 package br.com.example.zup_face.dto;
 
 import br.com.example.zup_face.enums.Cargo;
+import jdk.jfr.Name;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 public class UsuarioDto {
 
+    @Email(message = "Email é obrigatório")
     private String email;
+    @NotBlank(message = "nome é obrigatorio")
     private String nome;
     private String sobrenome;
     private Cargo cargo;
